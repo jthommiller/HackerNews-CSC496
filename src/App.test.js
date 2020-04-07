292997f636ab8139dd7a002fcd165edcd6fe79e2
James Miller <jthommiller@yahoo.com>
2020-04-06 17:19:42 -0700

    Updated tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
 import {shallow} from 'enzyme';
 
 it('renders learn react link', () => {
