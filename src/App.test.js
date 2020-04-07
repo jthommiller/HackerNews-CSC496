f8a68937d3a2201af38e5f6f239afaa652ad7817
James Miller <jthommiller@yahoo.com>
2020-04-06 17:43:43 -0700

    Updated test files

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-configure({ adapter: new Adapter() });
+
 import {shallow} from 'enzyme';
 
 it('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = shallow(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElement = shallow(<App/>);
+  expect(linkElement.find('a').getText()).toEqual('Learn React')
 });
 
 it('renders image', () => {
-  const image = shallow(<img/>)
-  expect(image).toBeInTheDocument();
+  const wrapper = shallow(<App/>);
+  expect(wrapper.find('img')).toBeInTheDocument();
 })
