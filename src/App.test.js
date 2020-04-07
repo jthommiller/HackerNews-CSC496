fa81396974d1ba85e54974e2f899893c1d9906ec
James Miller <jthommiller@yahoo.com>
2020-04-06 17:46:34 -0700

    Updated test files

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
-
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import {shallow} from 'enzyme';
+
+configure({ adapter: new Adapter() });
 
 it('renders learn react link', () => {
   const linkElement = shallow(<App/>);
