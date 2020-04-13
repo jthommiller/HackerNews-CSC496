db4d8c3de68e599d10d851af3663adeb0d8eb0ff
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-13 20:03:49 +0000

    Deploy jthommiller/HackerNews-CSC496 to github.com/jthommiller/HackerNews-CSC496.git:gh-pages

---
+++ precache-manifest.41fb343cd6bf5fdd7b6fb7c47232fd51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "547502c9222f73dccacb3cfc78488e5e",
+    "url": "/HackerNews-CSC496/index.html"
+  },
+  {
+    "revision": "79c254bcd2a325c9eb10",
+    "url": "/HackerNews-CSC496/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a6746d33f6ed3db6a232",
+    "url": "/HackerNews-CSC496/static/js/2.a9d4d54a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HackerNews-CSC496/static/js/2.a9d4d54a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79c254bcd2a325c9eb10",
+    "url": "/HackerNews-CSC496/static/js/main.2f6b5bcb.chunk.js"
+  },
+  {
+    "revision": "7b3a226523717946a839",
+    "url": "/HackerNews-CSC496/static/js/runtime-main.88675d34.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/HackerNews-CSC496/static/media/logo.5d5d9eef.svg"
+  }
+]);
